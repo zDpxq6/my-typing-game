a2095179b97b131d0356a85985f79136ec6b5104
tsuguka.hatanaka@jp.ricoh.com <tsuguka.hatanaka@jp.ricoh.com>
2024-12-30 04:14:52 +0900

    Set base path for deployment and copy index.html to 404.html on build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { copyFileSync } from "node:fs";
+import { join } from "node:path";
 
 declare module "@remix-run/node" {
   interface Future {
@@ -9,6 +11,7 @@ declare module "@remix-run/node" {
 }
 
 export default defineConfig({
+  base: "/my-typing-game/",
   plugins: [
     remix({
       ssr: false,
@@ -18,6 +21,15 @@ export default defineConfig({
         v3_throwAbortReason: true,
         v3_singleFetch: true,
         v3_lazyRouteDiscovery: true,
+      },
+      basename: "/my-typing-game/",
+      buildEnd(args) {
+        if (!args.viteConfig.isProduction) return;
+        const buildPath = args.viteConfig.build.outDir;
+        copyFileSync(
+          join(buildPath, "index.html"),
+          join(buildPath, "404.html")
+        );
       },
     }),
     tsconfigPaths(),
